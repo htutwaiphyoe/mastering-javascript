3a3531bb047eb9f7fb95a5501e76938a83b263ba
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-06 20:06:06 +0630

    Understanding Try/Catch/Finally

--- Chapter5.js
+++ Chapter5.js
@@ -456,3 +456,139 @@ function factorial(x) {
   return f;
 }
 console.log(factorial(4)); // => 24
+
+/*****************************
+ * 5.5.7 try/catch/finally
+ */
+
+try {
+  // Normally, this code runs from the top of the block to the bottom
+  // without problems. But it can sometimes throw an exception,
+  // either directly, with a throw statement, or indirectly, by calling
+  // a method that throws an exception.
+} catch (e) {
+  // The statements in this block are executed if, and only if, the try
+  // block throws an exception. These statements can use the local variable
+  // e to refer to the Error object or other value that was thrown.
+  // This block may handle the exception somehow, may ignore the
+  // exception by doing nothing, or may rethrow the exception with throw.
+} finally {
+  // This block contains statements that are always executed, regardless of
+  // what happens in the try block. They are executed whether the try
+  // block terminates:
+  // 1) normally, after reaching the bottom of the block
+  // 2) because of a break, continue, or return statement
+  // 3) with an exception that is handled by a catch clause above
+  // 4) with an uncaught exception that is still propagating
+}
+
+try {
+  // Ask the user to enter a number
+  let n = Number(prompt("Please enter a positive integer", ""));
+  // Compute the factorial of the number, assuming the input is valid
+  let f = factorial(n);
+  // Display the result
+  alert(n + "! = " + f);
+} catch (ex) {
+  // If the user's input was not valid, we end up here
+  alert(ex); // Tell the user what the error is
+}
+
+function show() {
+  try {
+    return 1;
+  } finally {
+    console.log("This is work");
+  }
+}
+console.log(show());
+
+function show2() {
+  try {
+    throw new Error("this doesn't work");
+  } catch (e) {
+    return e.message;
+  } finally {
+    console.log("This is work");
+  }
+}
+console.log(show2());
+
+function show3() {
+  try {
+    throw new Error("this doesn't work");
+  } catch (e) {
+    return e.message;
+  } finally {
+    return "This is Finally"; //overwrite e.message
+  }
+}
+console.log(show3());
+
+function show4() {
+  try {
+    throw new Error("this doesn't work");
+  } finally {
+    return "This is Finally"; //overwrite e.message
+  }
+}
+console.log(show4());
+
+// this while behaves correctly like for loop
+function show5() {
+  let i = 0;
+  while (i < 5) {
+    try {
+      if (i === 2) {
+        continue;
+      }
+      console.log(i);
+    } finally {
+      i++;
+    }
+  }
+}
+
+show5();
+
+function show6() {
+  let i = 0;
+  while (i < 5) {
+    try {
+      if (i === 2) {
+        break;
+      }
+      console.log(i);
+    } finally {
+      i++;
+      console.log(i);
+    }
+  }
+}
+
+show6();
+
+function show7() {
+  for (let i = 0; i < 5; i++) {
+    try {
+      if (i === 2) {
+        break;
+      }
+      console.log(i);
+    } finally {
+      console.log(i);
+    }
+  }
+}
+
+show7();
+
+function show8() {
+  try {
+    console.log(j);
+  } catch {
+    console.log("SOMETHING WENT WRONG"); // NO EXCEPTION CAN BE HANDLED
+  }
+}
+
+show8();
