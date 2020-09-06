fb6135891d38f9c0f7b98379eae4202251d7c6ba
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-06 19:16:05 +0630

    Understanding Throw

--- Chapter5.js
+++ Chapter5.js
@@ -442,3 +442,17 @@ function* range(from, to) {
 }
 
 range(0, 10);
+
+/*****************************
+ * 5.5.6 throw
+ */
+
+function factorial(x) {
+  // If the input argument is invalid, throw an exception!
+  if (x < 0) throw new Error("x must not be negative");
+  // Otherwise, compute a value and return normally
+  let f;
+  for (f = 1; x > 1; f *= x, x-- /* empty */);
+  return f;
+}
+console.log(factorial(4)); // => 24
