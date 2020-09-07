3e6be9eb0c0d4f4ebf76c238d1c0604948dc178a
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-07 14:13:31 +0630

    Understanding with

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 /*****************************
  * 5.6 Miscellaneous Statements
  */
+
+/*****************************
+ * 5.6.1 with
+ */
+let person = {
+  name: {},
+  address: {},
+  email: {},
+};
+with (person) {
+  // Access form elements directly here. For example:
+  name.value = "";
+  address.value = "";
+  email.value = "";
+  let a = "IK"; // this is oridinay variable
+  console.log(a);
+}
+
+console.log(person);
+
+let person2 = {
+  name: {},
+  address: {},
+  email: {},
+};
+person2.name.value = "";
+person2.address.value = "";
+person2.email.value = "";
+console.log(person2);
