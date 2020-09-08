ed6c16580fc469ea78ea8bd0b4feddc974f559df
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-08 08:24:10 +0630

    Understanding Variable Declaration

--- index.js
+++ index.js
@@ -5,11 +5,18 @@
 /*****************************
  * 5.7.1 const, let, and var
  */
+const TAU = 2 * Math.PI;
+let radius = 3;
+var circumference = TAU * radius;
 
 /*****************************
  * 5.7.2 function
  */
 
+console.log(area(10)); // HOISTED
+function area(radius) {
+  return Math.PI * radius * radius;
+}
 /*****************************
  * 5.7.3 class
  */
