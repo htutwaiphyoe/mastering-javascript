f6250f48731b9a6b772d3c7007a280901956b18e
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-09 09:05:57 +0630

    6.2.2

--- index.js
+++ index.js
@@ -39,3 +39,12 @@ console.log(empty, point, p2, book);
 /************************************
  * 6.2.2 Creating Objects with new
  */
+
+let o = new Object(); // Create an empty object: same as {}.
+let a = new Array(); // Create an empty array: same as [].
+let d = new Date(); // Create a Date object representing the current time
+let r = new Map(); // Create a Map object for key/value mapp
+
+/************************************
+ * 6.2.3 Prototypes
+ */
