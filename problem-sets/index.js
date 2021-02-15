737afc3e4c7d9bc40c7e22f98dfaa0f85d2c7c74
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-02-15 06:39:05 +0630

    #add: problem set - find odd or even number without conditional statement finished

--- problem-sets/index.js
+++ problem-sets/index.js
@@ -4,3 +4,14 @@ function sum(num1, num2) {
     return -(-num1 - num2);
 }
 console.log(sum(1, 2));
+
+// find odd or even number without conditional statement
+
+function checkOdd(num) {
+    let isOdd = false;
+    for (let i = 0; i < num % 2; i++) {
+        isOdd = !isOdd;
+    }
+    return isOdd;
+}
+console.log(checkOdd(2));
