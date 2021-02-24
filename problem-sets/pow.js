14f28f82047445d2991d72338c8f98f75cbc5da5
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-02-24 11:19:45 +0630

    #add: problem set - Two Sum (leetcode)

--- problem-sets/pow.js
+++ problem-sets/pow.js
@@ -1,24 +1,29 @@
 // implement power function without multiplication & division operators
 
 function pow(base, power) {
-    if (power === 0 || base === 1) {
+    let b = Math.abs(base);
+    if (power === 0 || b === 1) {
         return 1;
     } else if (power === 1) {
-        return base;
+        return b;
+    } else if (b === 0 && power < 0) {
+        return Infinity;
     } else {
-        let n = base;
-        let b = base;
+        let n = b;
+        let r = b;
         for (let j = 1; j < Math.abs(power); j++) {
             for (let i = 1; i < n; i++) {
-                b += base;
+                r += b;
             }
-            base = b;
+            b = r;
         }
 
-        if (power > 1) return base;
-        else return Math.exp(Math.log(1) - Math.log(base));
+        if (power > 1) return base < 0 && Math.abs(power) % 2 === 1 ? -b : b;
+        else
+            return base < 0 && Math.abs(power) % 2 === 1
+                ? -Math.exp(Math.log(1) - Math.log(b))
+                : Math.exp(Math.log(1) - Math.log(b));
     }
 }
 
-console.log(pow(2, 10));
-console.log(pow(2, -10));
+console.log(pow(-2, -3));
