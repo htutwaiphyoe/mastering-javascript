ebb47ef20db9036948249dfe11546f49d0096980
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-01 23:56:04 +0630

    #add: awesome tricks: every & some

--- tips-and-tricks/awesome/index.js
+++ tips-and-tricks/awesome/index.js
@@ -4,3 +4,8 @@ const array = [1, 2, 3, 1, 2, 3, 1, 2, 3];
 const uniqueArray = [...new Set(array)];
 
 console.log(uniqueArray);
+
+//  Array Methods: Every & Some
+const nums = [1, 30, 39, 29, 10, 13];
+console.log(nums.every((num, i, arr) => num < 40));
+console.log(nums.some((num, i, arr) => num < 5));
