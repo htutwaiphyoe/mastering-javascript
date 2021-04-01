5ea3e1f189f3f40f2cbc7a4efe98e6ffd0cf52b4
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-04-01 23:58:53 +0630

    #add: awesome tricks: convert to boolen

--- tips-and-tricks/awesome/index.js
+++ tips-and-tricks/awesome/index.js
@@ -1,11 +1,15 @@
 // Filter Unique Values
 const array = [1, 2, 3, 1, 2, 3, 1, 2, 3];
-
 const uniqueArray = [...new Set(array)];
-
 console.log(uniqueArray);
 
 //  Array Methods: Every & Some
 const nums = [1, 30, 39, 29, 10, 13];
 console.log(nums.every((num, i, arr) => num < 40));
 console.log(nums.some((num, i, arr) => num < 5));
+
+// Convert to Boolean
+const isTrue = !"";
+const isFalse = !"true";
+const alsoTrue = !!"true";
+console.log(isTrue, isFalse, alsoTrue);
